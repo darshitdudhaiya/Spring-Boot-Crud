d1492df4cf9180773e28c59c4e8d301e8d4ea909
darshitdudhaiya <darshitdudhaiya201@gmail.com>
2024-07-30 23:46:39 +0530

    Initial Commit

---
+++ journalapplication/src/test/java/com/journalapplication/journalapplication/JournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.journalapplication.journalapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
